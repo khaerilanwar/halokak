fe3474c1016beb0a21df69b4e675dc38db4a3c41
khaerilanwar <khaerilanwar1992@gmail.com>
2023-08-18 23:02:57 +0700

    add model rps

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,36 +1,107 @@
-// KODE JS UNTUK TOMBOL DARK MODE
-const checkbox = document.querySelector('#toggle');
-const tooltip = document.querySelector('#tooltip-left');
-const html = document.querySelector('html');
-const ikon = document.querySelector('#ikon');
+let model;
+const webcam = new Webcam(document.getElementById('wc'));
+let isPredicting= false;
 
-checkbox.addEventListener('change', function(event) {
-    // Mendapatkan status checked checkbox
-    var isChecked = event.target.checked;
+async function loadModel() {
+    const model = await tf.loadLayersModel('/assets/model/rps/model.json');
+    return model;
+}
 
-    // Melakukan sesuatu ketika status checked berubah
-    if (isChecked) {
-        console.log('Checkbox dicentang');
-        sessionStorage.setItem('darkMode', 'true');
-        console.log(sessionStorage.getItem('darkMode'));
-        // Lakukan tindakan yang diinginkan ketika checkbox dicentang
-    } else {
-        console.log('Checkbox tidak dicentang');
-        sessionStorage.removeItem('darkMode');
-        sessionStorage.setItem('darkMode', 'false');
-        console.log(sessionStorage.getItem('darkMode'));
-        // Lakukan tindakan yang diinginkan ketika checkbox tidak dicentang
+async function predict() {
+    while (isPredicting) {
+        const predictedClass = tf.tidy(() => {
+            const img = webcam.capture();
+            const predictions = model.predict(img);
+            return predictions.as1D().argMax();
+        })
+    
+        const classId = (await predictedClass.data())[0];
+        console.log(classId);
+        const viewPred = document.getElementById('view-predict');
+        viewPred.innerText = `Prediksi : ${classId}`;
+        predictedClass.dispose();
+        await tf.nextFrame();
     }
+}
 
-    if (sessionStorage.getItem('darkMode') === 'true') {
-        // Terapkan gaya dark mode
-        html.classList.add('dark');
-        tooltip.innerHTML = 'light';
-        ikon.classList.replace('fa-moon', 'fa-lightbulb');
-    } else {
-        // Terapkan gaya light mode
-        ikon.classList.replace('fa-lightbulb', 'fa-moon');
-        tooltip.innerHTML = 'dark';
-        html.classList.remove('dark');
+function startPredicting() {
+    isPredicting = true;
+    predict();
+}
+
+function stopPredicting() {
+    isPredicting = false;
+    predict();
+}
+
+async function init() {
+    await webcam.setup();
+    model = await loadModel();
+}
+
+// EKSPERIMENTAL USE ML API
+const video = document.getElementById('wc');
+const canvas = document.getElementById('view-image');
+const captureButton = document.getElementById('startPredicting');
+
+async function setupWebcam() {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+}
+
+async function sendImageToApi(base64Image) {
+    const apiUrl = 'https://kakanwar.pythonanywhere.com/rps';
+    const formData = new FormData();
+    formData.append('gambar', base64Image);
+
+    try {
+        document.getElementById('loading').classList.remove("hidden");
+        document.getElementById('loading').classList.add("flex");
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            body: formData,
+        });
+        
+        const data = await response.json();
+        document.getElementById('loading').classList.remove("flex");
+        document.getElementById('loading').classList.add("hidden");
+        document.getElementById("hasil-pred").innerHTML = data['label'];
+        console.log(data['label']);
+    } catch (error) {
+        console.error('Error sending image to API:', error);
     }
+}
+
+
+captureButton.addEventListener('click', async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+
+    video.onloadedmetadata = () => {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        const img_b64 = canvas.toDataURL('image/jpeg');
+        const image_data = img_b64.split(',')[1];
+
+        sendImageToApi(image_data);
+    };
 });
+
+function cobaApi() {
+    const Http = new XMLHttpRequest();
+    let data = new FormData();
+    const url='https://kakanwar.pythonanywhere.com/kampus';
+    // const url='http://localhost:5000/kampus';
+    Http.open("POST", url);
+    data.append('alamat', 'tegal');
+    Http.send(data);
+
+    Http.onreadystatechange = (e) => {
+    console.log(Http.responseText)
+    }
+}
+
+
+setupWebcam();
